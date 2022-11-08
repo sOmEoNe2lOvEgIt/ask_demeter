5c16aad7c3b8511abb1e9303c096813a49d3f155
sOmEoNe2lOvE <raymcraft@gmail.com>
2022-11-08 15:15:58 +0100

    Added alphabetical sorting for infiniband logs.

--- src/display/display_ib_logs.c
+++ src/display/display_ib_logs.c
@@ -11,21 +11,31 @@ int display_ib_logs(parsed_hostname_json_t *parsed_json)
 {
     if (!parsed_json)
         return (1);
-    printf ("%s LOGS:\n%s\n", parsed_json->hostname, parsed_json->ib_logs);
+    printf("\n\n\n");
+    print_line(100, false);
+    printf ("\t\t%s INFINIBAND CARD LOGS:\n", parsed_json->hostname);
+    print_line(100, false);
+    printf ("%s", parsed_json->ib_logs);
+    print_line(100, false);
     return (0);
 }
 
-int display_ib_logs_all_nodes(linked_list_t *parsed_json_list)
+int display_ib_logs_all_nodes(linked_list_t *host_list)
 {
     parsed_hostname_json_t *parsed_json = NULL;
+    linked_list_t *tmp = NULL;
+    int size = 0;
 
-    if (!parsed_json_list)
+    if (!host_list)
         return (1);
-    while (parsed_json_list) {
-        parsed_json = (parsed_hostname_json_t *)parsed_json_list->data;
+    size = get_list_size(host_list);
+    for (int i = 0; i < size; i++) {
+        if (!(tmp = get_first_host_after_hostname(host_list, parsed_json ? parsed_json->hostname : "")))
+            continue;
+        if (!(parsed_json = (parsed_hostname_json_t *)tmp->data))
+            continue;
         if (display_ib_logs(parsed_json))
             return (1);
-        parsed_json_list = parsed_json_list->next;
     }
     return (0);
 }
